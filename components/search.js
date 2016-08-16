a66d2a3861217c807e249c55139832a9595cb9b1
Maxine Ellah <maxine.ellah@gmail.com>
2016-08-17 11:14:07 +1200

    add api key for movie db and search input form

--- components/search.js
+++ components/search.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import key from '../keys.js'
 
 const Search = React.createClass({
   render: function () {
-    return <div><input type="text" placeholder="Actor"/><input type="text" placeholder="Genre"/><input type="text" placeholder="Length"/><input type="submit" name="Search"/></div>
+    return <div><form action="https://api.themoviedb.org" method="GET"><input type="text" placeholder="Actor"/><input type="text" placeholder="Genre"/><input type="text" placeholder="Length"/><input type="submit" value="Submit"/></form></div>
   }
 });
 
