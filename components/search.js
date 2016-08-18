d381a79f00cd7c4935bbdde34b62241e06a5ab9d
Maxine Ellah <maxine.ellah@gmail.com>
2016-08-18 17:27:34 +1200

    add watchify to package.json

--- components/search.js
+++ components/search.js
@@ -3,49 +3,80 @@ import key from '../keys.js'
 import Request from 'superagent';
 
 
+class Search extends React.Component {
 
-const Search = React.createClass({
-
-
-
-
-  render: function () {
-    return <div>
-    <select>
-      <option value="28">Action</option>
-      <option value="12">Adventure</option>
-      <option value="16">Animation</option>
-      <option value="35">Comedy</option>
-      <option value="80">Crime</option>
-      <option value="99">Documentary</option>
-      <option value="18">Drama</option>
-      <option value="10751">Family</option>
-      <option value="14">Fantasy</option>
-      <option value="10769">Non-english</option>
-      <option value="36">History</option>
-      <option value="27">Horror</option>
-      <option value="10402">Music</option>
-      <option value="9648">Mystery</option>
-      <option value="10749">Romance</option>
-      <option value="878">Science Fiction</option>
-      <option value="10770">TV Movie</option>
-      <option value="53">Thriller</option>
-      <option value="10752">War</option>
-      <option value="37">Western</option>
-    </select>
-
-    <input ref="Genre" type="text" placeholder="Genre"/>
-    <input ref="Rating" type="text" placeholder="Rating"/>
-    <input ref="Type" type="text" placeholder="Type"/>
-    <input type="submit" value="Submit"/>
-    </div>
+  constructor(){
+    super();
+    this.state = {};
   }
 
-  // search(Genre = "", Rating = "", Type = ""){
-  //   var url = `https://api.themoviedb.org${key}`
-  // }
+  componentWillMount(){
+    this.search();
+  }
 
-});
+  render(){
+      console.log('this.state.movies', this.state.movies)
+      var movies = this.state.movies;
+      console.log('movies', movies)
+      movies.map(function(movie) {
+        return <li>{movies.title}</li>
+      });
+      return <div>{movies}
+      </div>
+    };
+
+
+  search(){
+    var url = `http://api.themoviedb.org/3/discover/movie?${key}`;
+    Request.get(url).then((response) => {
+      console.log('response.body.results', response.body.results)
+      this.setState({
+        movies: response.body.results
+      });
+    });
+  }
+
+}
+
+
+
+  // render: function () {
+//     return <div>
+//     <select>
+//       <option ref="28" value="28">Action</option>
+//       <option ref="12" value="12">Adventure</option>
+//       <option value="16">Animation</option>
+//       <option value="35">Comedy</option>
+//       <option value="80">Crime</option>
+//       <option value="99">Documentary</option>
+//       <option value="18">Drama</option>
+//       <option value="10751">Family</option>
+//       <option value="14">Fantasy</option>
+//       <option value="10769">Non-english</option>
+//       <option value="36">History</option>
+//       <option value="27">Horror</option>
+//       <option value="10402">Music</option>
+//       <option value="9648">Mystery</option>
+//       <option value="10749">Romance</option>
+//       <option value="878">Science Fiction</option>
+//       <option value="10770">TV Movie</option>
+//       <option value="53">Thriller</option>
+//       <option value="10752">War</option>
+//       <option value="37">Western</option>
+//     </select>
+//
+//     <input ref="genre" type="text" placeholder="Genre"/>
+//     <input ref="rating" type="text" placeholder="Rating"/>
+//     <input ref="type" type="text" placeholder="Type"/>
+//     <input type="submit" value="Submit"/>
+//     </div>
+//   }
+//
+//
+// });
+
+
+
 
 
 
