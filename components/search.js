8e4d50fe187f62717d71d828fa0c3056eda3a9df
Maxine Ellah <maxine.ellah@gmail.com>
2016-09-03 17:58:21 +1200

    hard coded genre id input

--- components/search.js
+++ components/search.js
@@ -7,13 +7,18 @@ import Input from './input.js';
 let Search = React.createClass ({
   getInitialState(){
     return {
-      movies: ['Men In Black']
+      movies: ['Men In Black'],
+      genreValue: '12'
     };
   },
 
-  componentDidMount(){
-    console.log('compdidmount')
-    let genre = '';
+  // componentDidMount(){
+  //   console.log('compdidmount')
+  //
+  // },
+
+  getMovies(){
+    let genre = this.state.genreValue;
     let url   = `http://api.themoviedb.org/3/discover/movie?${key}&with_genres=${genre}`;
     Request.get(url).then((response) => {
       console.log('response.body.results', response.body.results)
@@ -25,11 +30,17 @@ let Search = React.createClass ({
     });
   },
 
+  handleGenre(newGenre) {
+    this.setState({ genreValue: newGenre })
+    this.getMovies();
+  },
+
   render(){
       console.log(this.state.movies)
+      console.log('genreValue state', this.state.genreValue)
       return (
         <div>
-          <Input/>
+          <Input genre={this.state.genreValue} onGenreChanged={this.handleGenre}/>
           <ul>
             {this.state.movies.map( function(movie){
               return <Results key={movie.id} data={movie}/>;
